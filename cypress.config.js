b4bb433a75c39d6ca43aec05a793d62e52fafbaf
AJ <andrzej.jurek.qa@gmail.com>
2024-03-26 23:42:31 +0100

    modified:   .gitignore 	modified:   cypress.config.js 	modified:   cypress/e2e/shetty/injectcookies.cy.js 	modified:   package-lock.json 	modified:   package.json

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,21 @@
 const { defineConfig } = require("cypress");
+const excelToJson = require('convert-excel-to-json');
+const fs = require('fs');
 
+async function setupNodeEvents(on, config) {
+  on('task', {
+    excelToJsonConverter(filePath) {
+      const result = excelToJson({
+        source: fs.readFileSync(filePath) // fs.readFileSync return a Buffer
+      });
+      return result;
+    }
+  })
+  return config;
+}
 module.exports = defineConfig({
   defaultCommandTimeout: 4000,
-  env:  {
+  env: {
     url: "https://rahulshettyacademy.com/",
     url2: 'https://todo.qacart.com/api/v1/tasks/',
   },
@@ -13,11 +26,6 @@ module.exports = defineConfig({
   },
   projectId: "kdkosg",
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      {
-        name: 'chrome'
-      }
+    setupNodeEvents
     },
-  },
 });
--- cypress/e2e/shetty/injectcookies.cy.js
+++ cypress/e2e/shetty/injectcookies.cy.js
@@ -1,16 +1,20 @@
+const excelToJson = require('convert-excel-to-json');
+const fs = require('fs');
+
+
 describe('Inject', () => {
-    it('cookies', () => {
+    it('cookies + excel file', () => {
         let productName;
-        let userEmail;
         cy.apiLogin().then(() => {
             cy.visit('https://rahulshettyacademy.com/client/', {
                 onBeforeLoad: function (window) {
                     window.localStorage.setItem('token', Cypress.env('token'))
-                    //userEmail = Cypress.env('userEmail');
                 }
             })
         })
-        console.log(userEmail)
+        cy.get(".card-body b").eq(1).then((el) => {
+            productName = el.text();
+        })
         cy.get('.card-body button:last-of-type').eq(1).click({ force: true }) //this can be changed into each
         cy.get("[routerlink*='cart']").click()
         cy.contains('Checkout').click()
@@ -22,10 +26,15 @@ describe('Inject', () => {
         })
         cy.contains('Place Order').click()
         cy.wait(2000)
-        cy.contains('Click To Download Order Details in Excel').click()
-        cy.readFile(Cypress.config('fileServerFolder') + '/cypress/downloads/order-invoice_anshika.csv').then(async (text) =>{
+        cy.get('.order-summary button').contains('Excel').click()
+        const filePath = Cypress.config("fileServerFolder") + "/cypress/downloads/order-invoice_anshika.xlsx"
+        let excelProductName;
+        cy.task('excelToJsonConverter', filePath).then((result) => {
+            excelProductName = result.data[1].B;
+            expect(productName).to.eq(excelProductName)
         })
-
-        //cy.readFile(Cypress.config('fileServerFolder') + '/cypress/downloads/order-invoice_' + userEmail + '.csv')
+        cy.readFile(filePath).then((text) => {
+            expect(text).to.include(productName)
+        })
     })
 })
